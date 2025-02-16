485ff283223efd434b052f655ac29fc31320f882
Akankobateng Rueben <104309954+ruebeniosdev@users.noreply.github.com>
2025-02-16 11:20:48 -0800

    [Feature] Made changes to the UI by removing paddings

--- Vodafone/Vodafone/ContentView.swift
+++ Vodafone/Vodafone/ContentView.swift
@@ -12,76 +12,66 @@ struct ContentView: View {
     @State private var password = ""
     var body: some View {
         NavigationView{
-            ZStack{
-            
-               
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: 350, height: 350)
-                    .cornerRadius(10)
-                    .padding(.bottom , 100)
-                
-                    .shadow(color: .black.opacity(0.3), radius: 20, x: 0.0, y: 10)
-                VStack{
+            VStack {
+                VStack {
                     Image("logo")
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 200 ,height: 100 )
-                        .padding(.bottom,600)
                 }
-                   
-                    VStack{
-                        
+                VStack(spacing: 20) {
+                    TextField("username or number", text: $Username)
+                        .padding()
+                        .foregroundColor(Color.red)
+                        .frame(width: 300, height: 60)
+                        .background(Color.white)
+                        .cornerRadius(10)
                     
-                        TextField("username or number", text: $Username)
-                            .padding()
-                            .foregroundColor(Color.red)
-                            .frame(width: 300, height: 60)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                        
-                        SecureField("password", text: $password)
-                            .padding()
-                            .foregroundColor(Color.red)
-                            .frame(width: 300, height: 60)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                        
-                        
+                    SecureField("password", text: $password)
+                        .padding()
+                        .foregroundColor(Color.red)
+                        .frame(width: 300, height: 60)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    HStack {
+                        Spacer()
+                        NavigationLink("Forgot password?", destination: Text("change password"))
+                            //  .padding([.top, .leading], 200.0)
+                            .foregroundColor(Color.white)
                     }
-                    .padding(.bottom, 200)
+                    .padding()
+                    
+                }
+                .padding()
+                .frame(width: 350, height: 250)
+                .background {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.red)
+                }
+                .shadow(color: .black.opacity(0.3), radius: 20, x: 0.0, y: 10)
                 
                 
-             
+                
                 NavigationLink(
                     destination: NavBar()
-                            .navigationBarBackButtonHidden(true).navigationBarHidden(true),
+                        .navigationBarBackButtonHidden(true).navigationBarHidden(true),
                     label: {
                         Text("Login")
                             .font(.headline)
-                            .frame(width: 300, height: 50, alignment: .center)
-                            .foregroundColor(.red)
-                            .background(Color.white)
+                            .frame(width: 350, height: 50, alignment: .center)
+                            .foregroundColor(.white)
+                            .background(Color.red)
                             .cornerRadius(10)
-                           .padding(.top, 10)
+                            .padding(.top, 10)
                     })
-                
-                
-                
-
-                NavigationLink("Forgot password?", destination: Text("change password"))
-                    .padding([.top, .leading], 200.0)
-                    .foregroundColor(Color.white)
-
-
-
-
+                VStack(spacing: 10) {
                     Text("Not registered with My Vodafone?")
-                    .padding(.top, 300)
-                        .foregroundColor(Color.black)
+                        .foregroundColor(Color.secondary)
                     NavigationLink("Register Now", destination: Registration())
-                        .padding(.top, 350)
                         .foregroundColor(Color.red)
-                
+                        .fontWeight(.bold)
+                }
+                .padding(.vertical)
             }
         }
     }
